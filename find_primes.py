f0f96afd4b14618873055e64b31ce620769e9d2d
mrdotx <klassiker@gmx.de>
2021-03-18 13:05:03 +0100

    multiprocessing added

--- find_primes.py
+++ find_primes.py
@@ -3,32 +3,68 @@
 path:   /home/klassiker/.local/share/repos/python/find_primes.py
 author: klassiker [mrdotx]
 github: https://github.com/mrdotx/python
-date:   2021-03-18T10:46:42+0100
+date:   2021-03-18T13:00:08+0100
 """
 
+import multiprocessing as mp
 import time
 
-START_NUMBER = 0
-END_NUMBER = 9999
-I_PRIMES = 0
+# 0 and 1 are not prime numbers
+START_NUMBER = 2
+END_NUMBER = 200000
 
-primes = []
-start = time.time()
+num_processes = mp.cpu_count() * 4
 
-print('primes between        : ' + str(START_NUMBER) + '-' + str(END_NUMBER))
+def chunks(seq, procs):
+    """
+    calculate chunks
+    """
+    size = len(seq)
+    start = 0
+    for i in range(1, procs + 1):
+        stop = i * size // procs
+        yield seq[start:stop]
+        start = stop
 
-for number in range(START_NUMBER, END_NUMBER, 1):
-    FOUND_PRIME = True
-    for div_number in range(2, number):
-        if number % div_number == 0:
-            FOUND_PRIME = False
-            break
-    if FOUND_PRIME:
-        primes.append(number)
-        I_PRIMES += 1
+def calc_primes(numbers):
+    """
+    prove that a number is a prime number
+    """
+    num_primes = 0
+    primes = []
 
-end = round(time.time() - start, 2)
+    for number in numbers:
+        found_prime = True
+        for div_number in range(2, number):
+            if number % div_number == 0:
+                found_prime = False
+                break
+        if found_prime:
+            primes.append(number)
+            num_primes += 1
+    return num_primes
 
-print('primes found          : ' + str(I_PRIMES))
-# print('primes                : ' + str(primes))
-print('\ntime elasped (seconds): ' + str(end))
+def main():
+    """
+    main function
+    """
+    print('cpu processes      : ' + str(num_processes))
+    print('primes between     : ' + str(START_NUMBER) + '-' + str(END_NUMBER))
+
+    start = time.time()
+
+    pool = mp.Pool(num_processes)
+
+    parts = chunks(range(START_NUMBER, END_NUMBER, 1), num_processes)
+    results = pool.map(calc_primes, parts)
+    total_primes = sum(results)
+
+    pool.close()
+
+    end = round(time.time() - start, 3)
+
+    print('primes found       : ' + str(total_primes))
+    print('time elasped (sec) : ' + str(end))
+
+if __name__ == "__main__":
+    main()
